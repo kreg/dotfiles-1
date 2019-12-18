58d67e0d53a9d1bfe2cdaacbc88f757babc5364c
Craig McDaniel <cmcdaniel@pindrop.com>
2019-12-18 14:33:23 -0500

    prompt changes

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,69 +9,40 @@ else
   git="/usr/bin/git"
 fi
 
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-# This assumes that you always have an origin named `origin`, and that you only
-# care about one specific origin. If this is not the case, you might want to use
-# `$git cherry -v @{upstream}` instead.
-need_push () {
-  if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
-  then
-    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
-
-    if [[ $number == 0 ]]
-    then
-      echo " "
-    else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
-    fi
-  fi
+user_and_host_name() {
+    echo "%{$fg[red]%}%n%{$fg[white]%}@%{$fg[green]%}%m%{$reset_color%}"
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+    echo "%{$fg[blue]%}%~%{$reset_color%}"
 }
 
-battery_status() {
-  if test ! "$(uname)" = "Darwin"
+git_status () {
+  if $(! $git status -s &> /dev/null)
   then
-    exit 0
+      echo ""
+  else
+      ref=$($git symbolic-ref HEAD 2>/dev/null)
+      git diff --no-ext-diff --quiet || dirty="*"
+      echo "%{$fg[yellow]%}(${ref#refs/heads/}${dirty})%{$reset_color%}"
   fi
+}
 
-  if [[ $(sysctl -n hw.model) == *"Book"* ]]
+the_time () {
+    echo "%{$fg[cyan]%}%*%{$reset_color%}"
+}
+
+# Prints the current kubectl context if applicable
+kube_prompt() {
+  local context ns
+  if [[ $(kubectl config view | grep "contexts: null") ]]
   then
-    $ZSH/bin/battery-status
+    echo ""
+  else
+    context="$(kubectl config current-context 2>/dev/null)"
+    ns="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.namespace}")" 
+    echo " using namespace %{$fg_bold[yellow]%}${ns}%{$reset_color%} in context %{$fg_bold[magenta]%}${context}%{$reset_color%}"
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
-}
-
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
-}
+export PROMPT=$'$(user_and_host_name):$(directory_name) $(git_status) $(the_time)\n%# '
