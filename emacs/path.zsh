5e3c039b60aae82c89c8f7675a74159fd28c74c4
Craig McDaniel <cmcdaniel@pindrop.com>
2019-12-19 10:43:03 -0500

    more changes

---
+++ emacs/path.zsh
@@ -0,0 +1,5 @@
+case "$OSTYPE" in
+    darwin*)
+        export PATH=/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10:${PATH}
+        ;;
+esac
---
+++ python/path.zsh
@@ -0,0 +1,5 @@
+case "$OSTYPE" in
+    darwin*)
+        export PATH=${HOME}/Library/Python/2.7/bin:${PATH}
+        ;;
+esac
