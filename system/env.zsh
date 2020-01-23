c2b908c5a79a5267510318470acb64b9a81ca3ef
Craig McDaniel <cmcdaniel@pindrop.com>
2020-01-23 15:46:08 -0500

    add LC_ALL

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,9 @@
 # fix less to use raw characters, exit immediately if less than a page 
 export LESS=RFX
+
+# Seems to be necessary to set LC_ALL on OSX for blackbox_postdeploy
+case "$OSTYPE" in
+    darwin*)
+        export LC_ALL=$LANG
+        ;;
+esac
