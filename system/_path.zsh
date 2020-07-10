37967d2c3402ce9a47991e3728c867ee345904a1
Craig McDaniel <cmcdaniel@pindrop.com>
2020-07-10 10:34:48 -0400

    linux path

--- system/_path.zsh
+++ system/_path.zsh
@@ -5,6 +5,6 @@ case "$OSTYPE" in
         export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
         ;;
     *)
-        export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+        export PATH="$HOME/bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
         ;;
 esac
