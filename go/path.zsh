cbb333d466ef6c176f68faf14a682a8f9cd878c1
Craig McDaniel <cmcdaniel@pindrop.com>
2020-07-11 15:07:08 -0400

    fix go path

--- go/path.zsh
+++ go/path.zsh
@@ -1,3 +1,10 @@
-export GOROOT=/usr/local/opt/go/libexec
-export GOPATH=$HOME/go
-export PATH="$GOPATH/bin:$PATH"
+case "$OSTYPE" in
+    darwin*)
+        export GOROOT=/usr/local/opt/go/libexec
+        export GOPATH=$HOME/go
+        export PATH="$GOPATH/bin:$PATH"
+        ;;
+    *)
+        export PATH=$PATH:/usr/local/go/bin
+        ;;
+esac
