74a9770e84fbc9ed66332d9dcb2bf403f2ea0d07
Craig McDaniel <cmcdaniel@pindrop.com>
2020-08-04 14:58:05 -0400

    fix tmux + ssh-agent

--- tmux/ssh_session.zsh
+++
@@ -1,7 +0,0 @@
-# Sane SSH_AUTH_SOCK handling for tmux, so that new SSH agents
-# created by subsequent logons are still usable. From
-# https://gist.github.com/admackin/4507371.
-function _ssh_auth_save() {
-    ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh-auth-sock.$HOSTNAME"
-}
-alias tmux='_ssh_auth_save ; export HOSTNAME=$(hostname) ; tmux'
