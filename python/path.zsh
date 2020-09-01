e05f86f6b3657a9bbf248bbf4d8bad99cc70ddb5
Craig McDaniel <cmcdaniel@pindrop.com>
2020-09-01 19:11:46 +0000

    python -> python3 on linux

--- python/path.zsh
+++ python/path.zsh
@@ -2,4 +2,7 @@ case "$OSTYPE" in
     darwin*)
         export PATH=${HOME}/Library/Python/2.7/bin:${PATH}
         ;;
+    *)
+        # python->python3, pip->pip3, etc...
+        export PATH=/home/linuxbrew/.linuxbrew/opt/python@3.8/libexec/bin:${PATH}
 esac
