25e3297130c7c33de8b12d1b878932120620d79f
Craig McDaniel <cmcdaniel@pindrop.com>
2020-12-11 16:49:42 +0000

    fully remote emacs

--- emacs/aliases.zsh
+++ emacs/aliases.zsh
@@ -1,6 +1,7 @@
-alias e="emacsclient -a false"
+# alias e="TERM=xterm-direct emacsclient -a false"
+alias e="TERM=xterm-direct emacsclient"
 case "$OSTYPE" in
     linux*)
-        alias emacs="TERM=xterm-24bit emacs"
+        alias emacs="TERM=xterm-direct emacs"
         ;;
 esac
