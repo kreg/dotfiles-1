558abef8e00a5b71c5f191a0e0ffb101c07e19af
Craig McDaniel <cmcdaniel@pindrop.com>
2021-02-01 14:22:57 -0500

    remove unused virtualenv

--- python/path.zsh
+++ python/path.zsh
@@ -1,6 +1,6 @@
 case "$OSTYPE" in
     darwin*)
-        export PATH=${HOME}/Library/Python/2.7/bin:${PATH}
+        export PATH=/usr/local/opt/python@3.9/libexec/bin:${PATH}
         ;;
     *)
         # python->python3, pip->pip3, etc...
--- python/virtualenv.zsh
+++
@@ -1,7 +0,0 @@
-export PROJECT_HOME=$HOME/src
-export WORKON_HOME=$HOME/.virtualenvs
-case "$OSTYPE" in
-    darwin*)
-        source $HOME/Library/Python/2.7/bin/virtualenvwrapper.sh
-        ;;
-esac
