5d3160f5bd370cf517c11d6389bd3b2f2ade5c42
Craig McDaniel <cmcdaniel@pindrop.com>
2021-04-06 16:38:15 +0000

    add pyenv

--- python/path.zsh
+++ python/path.zsh
@@ -4,5 +4,9 @@ case "$OSTYPE" in
         ;;
     *)
         # python->python3, pip->pip3, etc...
-        export PATH=/home/linuxbrew/.linuxbrew/opt/python@3.8/libexec/bin:${PATH}
+        export PATH=/home/linuxbrew/.linuxbrew/opt/python@3.9/libexec/bin:${PATH}
 esac
+
+if command -v pyenv 1>/dev/null 2>&1; then
+    eval "$(pyenv init -)"
+fi
