6ba9b77dde353aedec0235669f42fd1f8009cc15
Craig McDaniel <cmcdaniel@pindrop.com>
2021-04-21 14:32:29 -0400

    remove pyenv

--- python/path.zsh
+++ python/path.zsh
@@ -6,7 +6,3 @@ case "$OSTYPE" in
         # python->python3, pip->pip3, etc...
         export PATH=/home/linuxbrew/.linuxbrew/opt/python@3.9/libexec/bin:${PATH}
 esac
-
-if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-fi
