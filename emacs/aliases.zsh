eb536b6d85a75ed96dbe07080d88bdd0de19ea0d
Craig McDaniel <cmcdaniel@pindrop.com>
2022-09-01 14:57:01 -0400

    no more need for 24-bit terminal hack for emacs

--- emacs/aliases.zsh
+++ emacs/aliases.zsh
@@ -1,11 +1,12 @@
-# alias e="TERM=xterm-direct emacsclient -a false"
-case "$OSTYPE" in
-    linux*)
-        alias e="TERM=xterm-direct emacsclient"
-        alias emacs="TERM=xterm-direct emacs"
-        ;;
-    darwin*)
-        alias e="TERM=xterm-24bit emacsclient"
-        alias emacs="TERM=xterm-24bit emacs"
-        ;;
-esac
+# case "$OSTYPE" in
+#     linux*)
+#         alias e="TERM=xterm-direct emacsclient"
+#         alias emacs="TERM=xterm-direct emacs"
+#         ;;
+#     darwin*)
+#         alias e=emacsclient
+#         # alias e="TERM=xterm-24bit emacsclient"
+#         # alias emacs="TERM=xterm-24bit emacs"
+#         ;;
+# esac
+alias e=emacsclient
